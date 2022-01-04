6084848376a20b4640eb2af2efb5b63507ed90e2
TheRealSpacewalker <azamasim05@gmail.com>
2022-01-04 12:32:07 +0500

    Some graphics

--- assembly/overworld_scripts/celestial_scripts/goldtree_meadows.s
+++ assembly/overworld_scripts/celestial_scripts/goldtree_meadows.s
@@ -13,30 +13,32 @@ TileScript_GoldtreeMeadows_BattleToStopPlayer:
 	lock
 	playsong 0x129 0x0
 	getplayerpos 0x51FE, 0x51FF
-	compare 0x51FE, 0x11
+	compare 0x51FF, 0x1C
 	if equal _goto OtherGirlAttacks
-	compare 0x51FE, 0x12
+	compare 0x51FF, 0x1D
 	if equal _goto OtherGirlAttacks
 	msgbox gText_GoldtreeMeadows_BattleToStopPlayer_GirlTowardsLeft_01, MSG_KEEPOPEN
 	closeonkeypress
 	checksound
 	sound 0xA
-	applymovement 0x2, m_JumpTowardsMeowth
+	applymovement 0x4, m_JumpTowardsMeowth
 	waitmovement 0x0
 	checksound
 	sound 0x5C
 	checksound
 	sound 0xA
-	applymovement 0x2, m_JumpAwayFromMeowth
+	applymovement 0x4, m_JumpAwayFromMeowth
 	waitmovement 0x0
 	pause 0xE
-	spriteface 0x1, DOWN
+	spriteface 0x3, LEFT
 	msgbox gText_GoldtreeMeadows_BattleToStopPlayer_BothGirls, MSG_KEEPOPEN
 	closeonkeypress
-	spriteface 0x1, RIGHT
+	spriteface 0x3, DOWN
 	fadedefault
-	applymovement PLAYER, m_StepDown
+	applymovement PLAYER, m_StepLeft
 	waitmovement 0x0
+	setvar 0x51FE, 0x0
+	setvar 0x51FF, 0x0
 	release
 	end
 
@@ -45,25 +47,27 @@ OtherGirlAttacks:
 	closeonkeypress
 	checksound
 	sound 0xA
-	applymovement 0x3, m_JumpTowardsPikachu
+	applymovement 0x5, m_JumpTowardsPikachu
 	waitmovement 0x0
 	checksound
 	sound 0x5C
-	applymovement 0x3, m_WalkAwayFromPikachu
+	applymovement 0x5, m_WalkAwayFromPikachu
 	waitmovement 0x0
 	pause 0xE
-	spriteface 0x4, DOWN
+	spriteface 0x6, LEFT
 	msgbox gText_GoldtreeMeadows_BattleToStopPlayer_BothGirls, MSG_KEEPOPEN
 	closeonkeypress
-	spriteface 0x4, LEFT
+	spriteface 0x6, UP
 	fadedefault
-	applymovement PLAYER, m_StepDown
+	applymovement PLAYER, m_StepLeft
 	waitmovement 0x0
+	setvar 0x51FE, 0x0
+	setvar 0x51FF, 0x0
 	release
 	end
 
-m_JumpTowardsMeowth: .byte jump_right, end_m
-m_JumpAwayFromMeowth: .byte jump_left, look_right, end_m
-m_JumpTowardsPikachu: .byte jump_left, end_m
-m_WalkAwayFromPikachu: .byte walk_right, look_left, end_m
-m_StepDown: .byte walk_down_very_slow, end_m
+m_JumpTowardsMeowth: .byte jump_down, end_m
+m_JumpAwayFromMeowth: .byte jump_up, look_down, end_m
+m_JumpTowardsPikachu: .byte jump_up, end_m
+m_WalkAwayFromPikachu: .byte walk_down, look_up, end_m
+m_StepLeft: .byte walk_left_very_slow, end_m
